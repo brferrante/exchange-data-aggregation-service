1238d361f07f93bc3164ca433fbbf1a9d89b8bc4
Brian Ferrante <brian.ferrante@madmobile.com>
2022-06-18 16:23:07 -0300

    Initial commit, scaffolding files

---
+++ src/main/java/com/tradesoft/exchangedataaggregation/ExchangeDataAggregationApplication.java
@@ -0,0 +1,13 @@
+package com.tradesoft.exchangedataaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeDataAggregationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeDataAggregationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradesoft/exchangedataaggregation/ExchangeDataAggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradesoft.exchangedataaggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeDataAggregationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
